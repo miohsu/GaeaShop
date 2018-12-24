973635184cc55fe472cc7f655d89e334b19fb65c
miohsu <kunieshow@163.com>
2018-12-24 10:54:25 +0800

    mysql

--- GaeaShop/settings.py
+++ GaeaShop/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'GaeaShop.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gaea_shop',
+        'USER': 'gaea',
+        'PASSWORD': '123',
+        'HOST': '192.168.1.74',
+        'PORT': '',
     }
 }
 
